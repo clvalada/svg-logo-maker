1af5a0a97e6023b1440d4b41f7899ae91fdfb0f2
clvalada <clvalada@gmail.com>
2023-10-24 11:49:53 -0400

    pseudo code

---
+++ svgGenerator.js
@@ -0,0 +1,16 @@
+function generateSVG(userInput) {
+    // Extract user input  
+    // Define SVG dimensions
+    const width = 300;
+    const height = 200;
+  
+    // Create the SVG content based on user choices
+  
+    // Define text and shape elements
+  
+  
+    // Combine text and shape elements into the SVG content
+  
+    // Close the SVG element
+  
+
